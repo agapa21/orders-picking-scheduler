6594ee41be27aeb2645882174c70a3d7529ead9c
agapa21 <apiszczek14@gmail.com>
2023-04-01 23:07:12 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String args[])
@@ -9,5 +11,13 @@ public class Main {
 
         StoreJsonReader storeJsonReader = new StoreJsonReader(args[0], store);
         OrdersJsonReader ordersJsonReader = new OrdersJsonReader(args[1], orders);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(store.pickersArray.length);
+        for (int i = 0; i < store.pickersArray.length; i++) {
+            //executorService.execute();
+        }
+        executorService.shutdown();
+
+
     }
 }
